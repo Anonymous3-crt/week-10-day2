e78006b6ea36ff98f1aba11475be55e8bb8fb775
charles senator solomon <senatorsolomon735@gmail.com>
2026-03-27 12:09:33 +0100

    question15

---
+++ question15.js
@@ -0,0 +1,10 @@
+let rainfall = [120, 85, 200, 60, 175, 140];
+let total = 0;
+
+for (let i = 0; i < rainfall.length; i++) {
+  total += rainfall[i];
+}
+
+let average = total / rainfall.length;
+
+console.log(average);
