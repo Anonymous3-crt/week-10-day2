d6a9ad2e3f57ca50a81b3b4b4627676702258040
charles senator solomon <senatorsolomon735@gmail.com>
2026-03-27 11:33:02 +0100

    question7

---
+++ question7.js
@@ -0,0 +1,5 @@
+let classlist = ["Amara", "Dele", "Tunde", "Ngozi", "Ibrahim", "Kemi"];
+
+for (let i = 0; i < classlist.length; i++) {
+  console.log("Student " + (i + 1) + ": " + classlist[i]);
+}
